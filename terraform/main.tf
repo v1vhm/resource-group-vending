c1b21e03180f382d5db8f721a3e933e5133378b3
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-08-18 21:20:54 +0100

    fix: set Port provider source and version

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.0"
     }
+    port = {
+      source  = "port-labs/port-labs"
+      version = ">= 2.0.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
 }
+
+provider "port" {}
 
 data "azurerm_subscription" "current" {}
 
@@ -29,6 +35,84 @@ module "environment" {
   product_name       = local.product_name
   product_identifier = local.product_identifier
   services           = local.services
+}
+
+resource "port_entity" "resource_group" {
+  blueprint  = "azure_resource_group"
+  identifier = module.environment.resource_group_id
+  title      = module.environment.resource_group_name
+
+  properties = {
+    id       = module.environment.resource_group_id
+    name     = module.environment.resource_group_name
+    location = module.environment.resource_group_location
+    tags     = module.environment.resource_group_tags
+  }
+
+  relations = {
+    subscription = lower(data.azurerm_subscription.current.id)
+  }
+
+  run_id = var.port_run_id
+}
+
+resource "port_entity" "storage_account" {
+  blueprint  = "azure_storage_account"
+  identifier = module.environment.storage_account_id
+  title      = module.environment.storage_account_name
+
+  properties = {
+    id                       = module.environment.storage_account_id
+    name                     = module.environment.storage_account_name
+    location                 = module.environment.storage_account_location
+    is_hns_enabled           = module.environment.storage_account_is_hns_enabled
+    primary_location         = module.environment.storage_account_primary_location
+    secondary_location       = module.environment.storage_account_secondary_location
+    allow_blob_public_access = module.environment.storage_account_allow_blob_public_access
+    tags                     = module.environment.storage_account_tags
+  }
+
+  relations = {
+    resource_group = module.environment.resource_group_id
+  }
+
+  run_id = var.port_run_id
+}
+
+resource "port_entity" "state_container" {
+  blueprint  = "azure_storage_container"
+  identifier = module.environment.state_file_container
+  title      = module.environment.state_container_name
+
+  properties = {
+    id   = module.environment.state_file_container
+    name = module.environment.state_container_name
+  }
+
+  relations = {
+    storage_account = module.environment.storage_account_id
+  }
+
+  run_id = var.port_run_id
+}
+
+resource "port_entity" "user_managed_identity" {
+  blueprint  = "azure_user_assigned_identity"
+  identifier = module.environment.user_managed_identity_id
+  title      = module.environment.user_managed_identity_name
+
+  properties = {
+    id        = module.environment.user_managed_identity_id
+    name      = module.environment.user_managed_identity_name
+    client_id = module.environment.user_managed_identity_client_id
+    tags      = module.environment.user_managed_identity_tags
+  }
+
+  relations = {
+    resource_group = module.environment.resource_group_id
+  }
+
+  run_id = var.port_run_id
 }
 
 output "deployment_environment" {
